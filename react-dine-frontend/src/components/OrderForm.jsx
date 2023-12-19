25a30d079a3e82759cfe659907167b740d17310a
Ville Haapaniemi <ville.haapaniemi2@gmail.com>
2023-12-19 22:13:17 +0200

    Added confimation Modal after submitting customer form

--- react-dine-frontend/src/components/OrderForm.jsx
+++ react-dine-frontend/src/components/OrderForm.jsx
@@ -1,12 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { CartContext } from "../contexts/CartContext";
 import axios from "axios";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { Box, Typography } from "@mui/material";
+import OrderConfirmationModal from "./OrderConfirmationModal";
 
 const OrderForm = () => {
+  const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
+  const [customerData, setCustomerData] = useState();
   const {
     handleSubmit,
     control,
@@ -15,15 +18,21 @@ const OrderForm = () => {
 
   const { getCartItemsIdAndQuantity } = useContext(CartContext);
 
-  const onSubmit = async (customerData) => {
-    // Merge customer data from form and Cart items from CartContext
+  const onSubmitCustomerForm = (customerData) => {
+    setCustomerData(customerData);
+    setIsConfirmationModalOpen(true);
+  };
+
+  // This function is called after OrderConfirmationModal is confirmed
+  const submitOrder = async () => {
+    setIsConfirmationModalOpen(false);
+    // Merge customer data and cart items
     const orderData = {
       order: {
         customer: customerData,
-        items: getCartItemsIdAndQuantity(),
+        items: getCartItemsIdAndQuantity, // Returns item array including only item Id's and quantities
       },
     };
-
     try {
       const response = await axios.post(
         "http://localhost:5000/api/orders",
@@ -35,79 +44,96 @@ const OrderForm = () => {
     }
   };
 
+  const handleCloseModal = () => {
+    setIsConfirmationModalOpen(false);
+  };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
-      <Controller
-        name="name"
-        control={control}
-        defaultValue=""
-        rules={{ required: { value: true, message: "Required" } }}
-        render={({ field }) => (
-          <TextField {...field} label="Name" variant="outlined" />
-        )}
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmitCustomerForm)}
+        noValidate
+        autoComplete="off"
+      >
+        <Controller
+          name="name"
+          control={control}
+          defaultValue=""
+          rules={{ required: { value: true, message: "Required" } }}
+          render={({ field }) => (
+            <TextField {...field} label="Name" variant="outlined" />
+          )}
+        />
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=""
+          rules={{
+            required: { value: true, message: "Required" },
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // Must be valid email address structure
+              message: "Invalid email address",
+            },
+          }}
+          render={({ field }) => (
+            <Box>
+              <TextField {...field} label="Email" variant="outlined" />
+              {errors["email"] && <p>{errors["email"].message}</p>}
+            </Box>
+          )}
+        />
+        <Controller
+          name="street"
+          control={control}
+          defaultValue=""
+          rules={{ required: { value: true, message: "Required" } }}
+          render={({ field }) => (
+            <TextField {...field} label="Street" variant="outlined" />
+          )}
+        />
+        <Controller
+          name="postal-code"
+          control={control}
+          defaultValue=""
+          rules={{
+            required: { value: true, message: "Required" },
+            pattern: { value: /^[0-9]+$/, message: "Only numbers are allowed" },
+          }}
+          render={({ field }) => (
+            <Box>
+              <TextField
+                {...field}
+                label="Postal Code"
+                variant="outlined"
+                type="number"
+              />
+              {errors["postal-code"] && (
+                <Typography>{errors["postal-code"].message}</Typography>
+              )}
+            </Box>
+          )}
+        />
+        <Controller
+          name="city"
+          control={control}
+          defaultValue=""
+          rules={{ required: { value: true, message: "Required" } }}
+          render={({ field }) => (
+            <TextField {...field} label="City" variant="outlined" />
+          )}
+        />
+        <Button type="submit" variant="contained">
+          SUBMIT
+        </Button>
+      </form>
+
+      <OrderConfirmationModal
+        isConfirmationModalOpen={isConfirmationModalOpen}
+        onCloseModal={handleCloseModal}
+        onConfirmOrder={submitOrder}
+        customerData={customerData}
       />
-      <Controller
-        name="email"
-        control={control}
-        defaultValue=""
-        rules={{
-          required: { value: true, message: "Required" },
-          pattern: {
-            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // Must be valid email address structure
-            message: "Invalid email address",
-          },
-        }}
-        render={({ field }) => (
-          <Box>
-            <TextField {...field} label="Email" variant="outlined" />
-            {errors["email"] && <p>{errors["email"].message}</p>}
-          </Box>
-        )}
-      />
-      <Controller
-        name="street"
-        control={control}
-        defaultValue=""
-        rules={{ required: { value: true, message: "Required" } }}
-        render={({ field }) => (
-          <TextField {...field} label="Street" variant="outlined" />
-        )}
-      />
-      <Controller
-        name="postal-code"
-        control={control}
-        defaultValue=""
-        rules={{
-          required: { value: true, message: "Required" },
-          pattern: { value: /^[0-9]+$/, message: "Only numbers are allowed" },
-        }}
-        render={({ field }) => (
-          <Box>
-            <TextField
-              {...field}
-              label="Postal Code"
-              variant="outlined"
-              type="number"
-            />
-            {errors["postal-code"] && (
-              <Typography>{errors["postal-code"].message}</Typography>
-            )}
-          </Box>
-        )}
-      />
-      <Controller
-        name="city"
-        control={control}
-        defaultValue=""
-        rules={{ required: { value: true, message: "Required" } }}
-        render={({ field }) => (
-          <TextField {...field} label="City" variant="outlined" />
-        )}
-      />
-      <Button type="submit" variant="contained">
-        SUBMIT
-      </Button>
-    </form>
+    </>
   );
 };
 
